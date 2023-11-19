43f8c10d7e58999a83796e4dcd6ac01638f56fe0
Ricaldone-Gabriel <gabriel.ricaldone@jcmaxwell.it>
2023-11-19 23:42:56 +0100

    Completamento

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ ws(app);
 
 const port = 8080;
 
+//Struttura database
+//accountdb
+//|-Accounts
+//  |-id
+//  |-username
+//  |-password
+//
+
 var connection = mysql.createConnection({
   host: "localhost", //"IPCasa:3306"
   user: "root",
@@ -59,15 +67,20 @@ app.get("/loginPage", isNotAuthenticated, (req, res) => {
   res.render("login.ejs");
 });
 
+app.get("/registerPage", isNotAuthenticated, (req, res) => {
+  res.render("register.ejs");
+});
+
 app.post("/login", isNotAuthenticated, (req, res) => {
   const { username, password, remember } = req.body;
-  query = "SELECT * FROM account WHERE Username = ? AND Password = ?";
+  query = "SELECT * FROM accounts WHERE username = ? AND password = ?";
 
   connection.query(query, [username, password], (err, result) => {
     if (err) throw err;
     if (result.length > 0) {
       const user = result[0];
-      req.session.user = user.Id;
+      req.session.user = user.id;
+      req.session.username = user.username;
 
       if (remember) {
         req.session.cookie.maxAge = 30 * 24 * 60 * 60 * 1000; // 30 giorni
@@ -76,6 +89,27 @@ app.post("/login", isNotAuthenticated, (req, res) => {
       }
 
       res.redirect("/");
+    } else {
+      res.redirect("/loginPage");
+    }
+  });
+});
+
+app.post("/register", isNotAuthenticated, (req, res) => {
+  const { username, password } = req.body;
+  query = "SELECT * FROM accounts WHERE username = ? AND password = ?";
+
+  connection.query(query, [username, password], (err, result) => {
+    if (err) throw err;
+    if (result.length > 0) {
+      res.redirect("/register");
+    } else {
+      query = `INSERT INTO accounts (username,password) VALUES ('${username}','${password}')`;
+      connection.query(query, (err, result) => {
+        if (err) throw err;
+        else console.log("Utente inserito");
+      });
+      res.redirect("/loginPage");
     }
   });
 });
@@ -84,118 +118,32 @@ const activeConnections = new Map();
 
 app.ws("/ws", (ws, req) => {
   const userId = req.session.user;
-  // Aggiungi i tuoi gestori di eventi WebSocket qui...
-  if (!clientId) {
-    // Se l'ID unico non è presente, chiudi la connessione
+  const username = req.session.username;
+  var message;
+
+  if (!userId) {
     ws.close();
     return;
   }
 
-  activeConnections.set(clientId, ws);
+  activeConnections.set(userId, { ws: ws, username: username });
 
-  ws.send(`Benvenuto! Il tuo ID è: ${clientId}`);
+  ws.on("message", (data) => {
+    Id = userId;
+    console.log(`Ricevuto messaggio da utente ${userId} di username: ${data}`);
+    activeConnections.forEach((client, id) => {
+      message = [username, data];
+      client.ws.send(JSON.stringify(message));
+    });
+  });
 
-  ws.on("message", (message) => {
-    console.log(`Ricevuto messaggio da utente ${userId}: ${message}`);
-    // Puoi fare qualcosa con l'ID utente, ad esempio inviare un messaggio solo a quel particolare utente
-    // ws.send(`Utente ${userId} ha detto: ${message}`);
+  ws.on("close", () => {
+    // Rimuovi la connessione dalla lista quando il client si disconnette
+    activeConnections.delete(userId);
+
+    console.log(`Connessione WebSocket chiusa per il client con ID ${userId}.`);
   });
 });
-
 app.listen(port, () => {
   console.log(`hey, listen! ${port}`);
 });
-
-/*
-const express = require('express');
-const expressWs = require('express-ws');
-
-const app = express();
-expressWs(app);
-
-// Lista delle connessioni attive con i rispettivi ID utente
-const activeConnections = new Map();
-
-app.ws('/ws', (ws, req) => {
-  // Genera un ID univoco per il nuovo client
-  const clientId = generateUniqueId();
-
-  // Associa l'ID utente alla connessione WebSocket
-  activeConnections.set(clientId, ws);
-
-  // Invia l'ID unico al client quando si connette
-  ws.send(JSON.stringify({ type: 'clientId', clientId }));
-
-  // Aggiungi altri gestori di eventi WebSocket qui...
-  console.log(`Client con ID ${clientId} collegato.`);
-
-  ws.on('message', (message) => {
-    console.log(`Ricevuto messaggio dal client ${clientId}: ${message}`);
-    // Puoi aggiungere la logica per gestire i messaggi inviati dal client
-  });
-
-  ws.on('close', () => {
-    // Rimuovi la connessione dalla lista quando il client si disconnette
-    activeConnections.delete(clientId);
-
-    console.log(`Connessione WebSocket chiusa per il client con ID ${clientId}.`);
-  });
-});
-
-function generateUniqueId() {
-  // Implementa la tua logica per generare un ID univoco (ad esempio, usando una libreria come 'uuid')
-  // In questo esempio, viene utilizzata una semplice combinazione di timestamp e un numero casuale
-  return Date.now().toString(36) + Math.random().toString(36).substr(2, 5);
-}
-
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server avviato su http://localhost:${port}`);
-});
-Lato client (HTML/JavaScript):
-
-html
-Copy code
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WebSocket Client</title>
-</head>
-<body>
-    <div>
-        A
-    </div>
-    <script>
-        const webSocket = new WebSocket('ws://localhost:3000/ws');
-
-        webSocket.addEventListener('open', (event) => {
-            console.log('Connessione WebSocket aperta.');
-        });
-
-        webSocket.addEventListener('message', (event) => {
-            const data = JSON.parse(event.data);
-
-            if (data.type === 'clientId') {
-                const clientId = data.clientId;
-                console.log(`Il tuo ID unico è: ${clientId}`);
-
-                // Puoi fare qualcosa con l'ID unico del client
-            } else {
-                // Altri tipi di messaggi possono essere gestiti qui
-                console.log('Messaggio dal server:', event.data);
-            }
-        });
-
-        webSocket.addEventListener('close', (event) => {
-            console.log('Connessione WebSocket chiusa.');
-        });
-
-        webSocket.addEventListener('error', (event) => {
-            console.error('Errore nella connessione WebSocket:', event);
-        });
-    </script>
-</body>
-</html>
-*/
